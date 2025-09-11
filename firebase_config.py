dc423f73ffeb0d8d0d180426444fccd5f9e24d7f
Jernish-FDO <jernishamala9@gmail.com>
2025-09-11 16:37:22 +0530

    fixed

--- firebase_config.py
+++ firebase_config.py
@@ -1,16 +1,16 @@
 FIREBASE_CONFIG = {
-    'apiKey': "AIzaSyCBIGUJhlT84xl1ra_s7DSmyiRIVbm3zbc",
-    'authDomain': "pico-w-d4b17.firebaseapp.com",
-    'databaseURL': "https://pico-w-d4b17-default-rtdb.asia-southeast1.firebasedatabase.app",
-    'projectId': "pico-w-d4b17",
-    'storageBucket': "pico-w-d4b17.firebasestorage.app",
-    'messagingSenderId': "292764097438",
-    'appId': "1:292764097438:web:f2ae7c10cf4dc98ca2d48f",
-    'deviceEmail': "amalajernish228@gmail.com",
-    'devicePassword': "Jer2005@"
+    'apiKey': "",
+    'authDomain': "",
+    'databaseURL': "",
+    'projectId': "",
+    'storageBucket': "",
+    'messagingSenderId': "",
+    'appId': "",
+    'deviceEmail': "",
+    'devicePassword': "
 }
 
 WIFI_CONFIG = {
-    'ssid': "JERNISH",
-    'password': "12346789"
+    'ssid': "",
+    'password': ""
 }
